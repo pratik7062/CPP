63c211ddebfc078ae11f2675e7fe2c963b121967
pratik jadav <103435083+pratik7062@users.noreply.github.com>
2022-04-11 20:21:47 +0530

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include<iostream.>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	cout<<"Hello world";
+	return 0;
+}
---
+++ simpleinterest.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int p,r,t,i;
+	cout<<"Enter P, R and T";
+	cin>>p>>r>>t;
+	i=p*r*t/100;
+	cout<<"\nInterest: "<<i;
+	return 0;
+	
+}
